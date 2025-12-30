f3d48ce7c0d028bdeee7b211007d46af5f1a5099
junios-study <junios@naver.com>
2025-12-30 15:02:59 +0900

    C++ 포팅 시작

---
+++ enc_temp_folder/cccfa0eec03d3a486c2d76deccfa/LobbyUserWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKSTUDY_API ULobbyUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	
+};
